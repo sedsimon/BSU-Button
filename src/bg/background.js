a74bb88b79c005d2f82c6cc25cc4ced41b14a2a3
sedsimon <simon.stanlake@gmail.com>
2014-05-08 17:54:30 -0700

    * check to only bsu posts that other people created

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,33 +3,33 @@ var fb = new Firebase('https://incandescent-fire-4279.firebaseio.com/');
 var lastItem = fb.endAt().limit(1);
 
 lastItem.on('child_added', function(snapshot) {
-  var bsu = snapshot.val();
-  chrome.storage.sync.get("UUID",function(obj) {
-      var myUUID = obj.UUID;
-    //  if (myUUID != bsu.UUID){
-      chrome.storage.sync.get("lastNotification", function(obj){
-	  var lastNotification = obj.lastNotification;
-	  if (lastNotification !== snapshot.name()){
-	      var notification = webkitNotifications.createNotification(
-		  '/icons/128px marketing owly.png',  // icon url - can be relative
-		  'New post available!',  // notification title
-		  'Click to #BSU' // notification body text
-	      );
+    var bsu = snapshot.val();
+    chrome.storage.sync.get("UUID",function(obj) {
+	var myUUID = obj.UUID;
+	if (myUUID != bsu.UUID){
+	    chrome.storage.sync.get("lastNotification", function(obj){
+		var lastNotification = obj.lastNotification;
+		if (lastNotification !== snapshot.name()){
+		    var notification = webkitNotifications.createNotification(
+			'/icons/128px marketing owly.png',  // icon url - can be relative
+			'New post available!',  // notification title
+			'Click to #BSU' // notification body text
+		    );
 
-	      notification.onclick = function() {
-		  window.open("https://hootsuite.com/hootlet/social-share?partner=bsunotification&url="
-			      + encodeURIComponent(bsu.url) +
-			      "&title="
-			      + encodeURIComponent(bsu.title));
-	      }
-	      notification.show();
-	      chrome.storage.sync.set({lastNotification:snapshot.name()},function(){});
-	  }
-     });
+		    notification.onclick = function() {
+			window.open("https://hootsuite.com/hootlet/social-share?partner=bsunotification&url="
+				    + encodeURIComponent(bsu.url) +
+				    "&title="
+				    + encodeURIComponent(bsu.title));
+		    }
+		    notification.show();
+		    chrome.storage.sync.set({lastNotification:snapshot.name()},function(){});
+		}
+	    });
 
-  //    }
+	}
 
- });
+    });
 });
 
 
